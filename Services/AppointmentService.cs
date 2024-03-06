4ff6e105c526603ba8ac88787e279f802def279d
Umidjon <makhammadsoliyev@gmail.com>
2024-03-06 19:10:44 +0500

    Add AppointmentService

---
+++ Services/AppointmentService.cs
@@ -0,0 +1,37 @@
+﻿using BarberShop.DbContexts;
+using BarberShop.Interfaces;
+using BarberShop.Models.Appointments;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarberShop.Services;
+
+public class AppointmentService : IAppointmentService
+{
+    private readonly BarbershopDbContext dbContext;
+    private readonly DbSet<Appointment> appointments;
+
+    public AppointmentService(BarbershopDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+        this.appointments = dbContext.Appointments;
+    }
+
+    public async Task<Appointment> BookAsync(Appointment appointment)
+    {
+        var entityAppointment = await appointments.AddAsync(appointment);
+
+        await dbContext.SaveChangesAsync();
+
+        return entityAppointment.Entity;
+    }
+
+    public async Task<bool> CancelAsync(long id)
+    {
+        var appointment = await appointments.FirstOrDefaultAsync(a => a.Id == id)
+            ?? throw new Exception();
+
+        appointments.Remove(appointment);
+
+        return true;
+    }
+}
