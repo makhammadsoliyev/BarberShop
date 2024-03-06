29b54dae9018c2fd06349bcecff78cd5fa53e2b3
Diyorbek <muhammadjonovd793@gmail.com>
2024-03-06 10:30:54 +0500

    Add User Model

---
+++ Models/Users/User.cs
@@ -0,0 +1,11 @@
+﻿using BarberShop.Models.Commons;
+
+namespace BarberShop.Models.Users;
+
+public class User : Auditable
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber {  get; set; }
+    public string Passenger { get; set; }
+}
