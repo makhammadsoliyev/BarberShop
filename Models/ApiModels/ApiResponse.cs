301c270d8e5edf73716003384bf5fea1136888a1
Umidjon <makhammadsoliyev@gmail.com>
2024-03-06 20:04:46 +0500

    Create ApiModels And GeoLocation Helper

---
+++ Models/ApiModels/ApiResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace BarberShop.Models.ApiModels;
+
+public class ApiResponse
+{
+    [JsonProperty("documentation")]
+    public string Documentation { get; set; }
+
+    [JsonProperty("results")]
+    public List<Location> Locations { get; set; }
+
+    [JsonProperty("total_results")]
+    public int TotalResults { get; set; }
+}
---
+++ Models/ApiModels/Geometry.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace BarberShop.Models.ApiModels;
+
+public class Geometry
+{
+    [JsonProperty("lat")]
+    public string Latitude { get; set; }
+
+    [JsonProperty("lng")]
+    public string Longitude { get; set; }
+}
