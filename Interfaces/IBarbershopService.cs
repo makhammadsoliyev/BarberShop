a0eaca10c5278def75cf3e29f31f9646d04589fb
Diyorbek <muhammadjonovd793@gmail.com>
2024-03-06 11:34:35 +0500

    Add IBarberShopService Interface

---
+++ Interfaces/IBarbershopService.cs
@@ -0,0 +1,14 @@
+﻿using BarberShop.Models.Appointments;
+using BarberShop.Models.Barbershops;
+
+namespace BarberShop.Interfaces;
+
+public interface IBarbershopService
+{
+    Task<Barbershop> CreateAsync(Barbershop barberShop);
+    Task<Barbershop> UpdateAsync(long id, Barbershop barbershop);
+    Task<bool> DeleteAsync(long id);
+    Task<Barbershop> GetByIdAsync(long id);
+    Task<IEnumerable<Barbershop>> GetAllAsync();
+    Task<IEnumerable<Appointment>> GetAllAppointmentsAsync();
+}
