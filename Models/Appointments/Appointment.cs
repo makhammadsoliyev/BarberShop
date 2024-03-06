9b3dea1d468a0010a048c2c9b452868b492d37ca
Umidjon <makhammadsoliyev@gmail.com>
2024-03-06 10:43:54 +0500

    Add Appointment Model

---
+++ Models/Appointments/Appointment.cs
@@ -0,0 +1,11 @@
+﻿using BarberShop.Models.Commons;
+
+namespace BarberShop.Models.Appointments;
+
+public class Appointment : Auditable
+{
+    public long UserId { get; set; }
+    public long BarberShopId { get; set; }
+    public DateOnly Date { get; set; }
+    public TimeOnly Time { get; set; }
+}
